1e4ed65126d590452c5df705d5c0bb6f13182116
thomasbalmant <thomas_dru@hotmail.com>
2022-10-06 19:05:54 -0300

    currency model

---
+++ lib/app/models/currency_model.dart
@@ -0,0 +1,9 @@
+class CurrencyModel {
+  final String name;
+  final double real;
+  final double dolar;
+  final double bitcoin;
+  final double euro;
+
+  CurrencyModel(this.name, this.real, this.dolar, this.bitcoin, this.euro);
+}
