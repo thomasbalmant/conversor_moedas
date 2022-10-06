cac110436a52d05ff09e403e13239e1ad5936e4d
thomasbalmant <thomas_dru@hotmail.com>
2022-10-06 19:10:25 -0300

    currencys

--- lib/app/models/currency_model.dart
+++ lib/app/models/currency_model.dart
@@ -5,5 +5,22 @@ class CurrencyModel {
   final double bitcoin;
   final double euro;
 
-  CurrencyModel(this.name, this.real, this.dolar, this.bitcoin, this.euro);
+  CurrencyModel(
+      {required this.name,
+      required this.real,
+      required this.dolar,
+      required this.bitcoin,
+      required this.euro});
+
+  static List<CurrencyModel> getCurrencies() {
+    return <CurrencyModel>[
+      CurrencyModel(
+        name: 'Real',
+        real: 1.0,
+        dolar: 0.18,
+        bitcoin: 0.000016,
+        euro: 0.15,
+      ),
+    ];
+  }
 }
